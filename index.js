c63bd5795c5db7cb4063f8de24ad0cc620077402
Gyanpatra <hi.gyanaranjanpatra@gmail.com>
2026-05-17 20:02:34 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.status(200).json({
+        message: "Hello from the server"
+    });
+});
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log(`Server is running at ${process.env.PORT || 3000}`)
+})
