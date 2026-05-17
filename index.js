56c66ae0de788ac7f02b2c550e67b1027bdefc82
Gyanpatra <hi.gyanaranjanpatra@gmail.com>
2026-05-17 22:31:19 +0530

    retry deploy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.get("/", (req,res)=>{
     res.status(200).json({
-        message: "Hello from the server v1"
+        message: "Hello from the server v69"
     });
 });
 
